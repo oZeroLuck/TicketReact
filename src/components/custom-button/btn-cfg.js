ca293b3e34a4e7d59302ce3b8ba5a68b997dc8e2
luigi.durso <luigi.durso@si2001.it>
2021-06-29 18:02:46 +0200

    Added button cfg for custom button

---
+++ src/components/custom-button/btn-cfg.js
@@ -0,0 +1,122 @@
+import {
+    faSave,
+    faEdit,
+    faHome,
+    faTrashAlt,
+    faUserEdit,
+    faSignInAlt,
+    faCarSide,
+    faArrowAltCircleLeft,
+    faBook,
+    faCheckCircle, faTimesCircle, faUser,
+    faSignOutAlt
+} from "@fortawesome/free-solid-svg-icons";
+
+const SaveBtn = {
+    customCssClass: 'success',
+    text: 'Save',
+    icon: faSave
+
+}
+
+const EditBtn = {
+    customCssClass : 'secondary',
+    text : 'Edit',
+    icon : faEdit,
+}
+
+const EditUserBtn = {
+    customCssClass : 'secondary',
+    text : 'Edit',
+    icon : faUserEdit,
+}
+
+const DeleteBtn = {
+    customCssClass : 'danger',
+    text : 'Delete',
+    icon : faTrashAlt,
+}
+
+const HomeBtn = {
+    customCssClass : 'warning',
+    text : 'Home',
+    icon : faHome,
+}
+
+const LoginBtn = {
+    customCssClass : 'success',
+    text : 'Login',
+    icon : faSignInAlt,
+}
+
+const LogoutBtn = {
+    customCssClass : 'danger',
+    text : 'Logout',
+    icon : faSignOutAlt,
+}
+
+const CarBtn = {
+    customCssClass : 'warning',
+    text : 'Car Park',
+    icon : faCarSide,
+}
+
+const BackBtn = {
+    customCssClass : 'warning',
+    text : 'Back',
+    icon : faArrowAltCircleLeft,
+}
+
+const BookBtn = {
+    customCssClass : 'success',
+    text : 'Book',
+    icon : faBook,
+}
+
+const ApproveBtn = {
+    customCssClass : 'success',
+    text : 'Approve',
+    icon : faCheckCircle,
+}
+
+const DenyBtn = {
+    customCssClass : 'danger',
+    text : 'Deny',
+    icon : faTimesCircle,
+}
+
+const ReservedAreaBtn = {
+    customCssClass : 'success',
+    text : 'Reserved Area',
+    icon : faUser,
+}
+
+const SignUpBtn = {
+    customCssClass : 'primary',
+    text : 'Sign Up',
+    icon : null
+}
+
+export {
+    SaveBtn,
+    EditBtn,
+    DeleteBtn,
+    EditUserBtn,
+    HomeBtn,
+    LoginBtn,
+    CarBtn,
+    BackBtn,
+    BookBtn,
+    ApproveBtn,
+    DenyBtn,
+    ReservedAreaBtn,
+    LogoutBtn,
+    SignUpBtn
+}
+/*
+const {
+    customCssClass: '',
+    text: '',
+    icon: ''
+}
+*/
