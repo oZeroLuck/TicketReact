3f46dcc999b14d0ee753598d801f696f744892ac
luigi.durso <luigi.durso@si2001.it>
2021-06-29 18:03:53 +0200

    Deleted logo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
+import {Homepage} from "./pages/homepage";
+import {Films} from "./pages/film-list";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <BrowserRouter>
+              <Switch>
+                <Route exact path="/">
+                    <Redirect to="/homepage"/>
+                </Route>
+                  <Route path="/homepage" component={Homepage}/>
+                  <Route path="/film" component={Films}/>
+              </Switch>
+          </BrowserRouter>
+      </div>
   );
 }
 
