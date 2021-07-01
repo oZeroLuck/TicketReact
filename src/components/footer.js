2d8bbcdfa300e803898f9558bd18242711e8f65d
luigi.durso <luigi.durso@si2001.it>
2021-07-01 18:14:46 +0200

    Fixed "footer" visual bug

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,10 +2,12 @@ import {Container, Row} from "react-bootstrap";
 
 function Footer() {
     return (
-        <Container fluid={"xl"}>
-        <Row style={{backgroundColor: "#343a40", color: "white"}}>
-            <p>Text text text</p>
-        </Row>
+        <Container fluid>
+            <Row style={{backgroundColor: "#343a40", color: "white"}}
+                 className={"pt-3 pl-4 pr-4"}
+            >
+                <p>Text text text</p>
+            </Row>
         </Container>
     )
 }
