6c895d4dd24c09da704c528387d9054e11e34a81
luigi.durso <luigi.durso@si2001.it>
2021-07-01 12:15:29 +0200

    Added possibility to chose alert style through props, defaults to success

--- src/components/custom-snackbar.js
+++ src/components/custom-snackbar.js
@@ -3,11 +3,20 @@ import './components.css'
 import {Alert, Fade} from "react-bootstrap";
 
 function CustomSnackbar(props) {
+    const type = "success"
+
+    function chooseType() {
+        if (props.type === null || props.type === undefined) {
+            return type
+        } else {
+            return props.type
+        }
+    }
 
         return (
             <Fade in={props.show} onExited={() => props.close}>
                 <div className={"center-me"}>
-                    <Alert show={props.show} variant="success">
+                    <Alert show={props.show} variant={chooseType()}>
                         <p>
                             {props.message}
                         </p>
