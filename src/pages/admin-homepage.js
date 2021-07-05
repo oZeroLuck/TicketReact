01742c165bcb455d0a08e2209aba27c745f5ec0d
luigi.durso <fabiolo97.hu@gmail.com>
2021-07-05 17:52:53 +0200

    Added temporary homepage

---
+++ src/pages/admin-homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class AdminHomepage extends React.Component {
+    render() {
+        return (
+            <div>
+                I'm Admin Homepage and I'm working
+            </div>
+        )
+    }
+}
+
+export {AdminHomepage}
