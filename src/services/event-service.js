32dc2b49ad3041cc254089f4264608ccd8588741
luigi.durso <fabiolo97.hu@gmail.com>
2021-07-06 18:07:08 +0200

    Added calls

--- src/services/event-service.js
+++ src/services/event-service.js
@@ -6,6 +6,14 @@ class EventService {
         return axios.get('http://localhost:8080/film/' + id, {timeout: 10000})
     }
 
+    getEvents() {
+        return axios.get('http://localhost:8080/event?', {timeout: 10000})
+    }
+
+    getLocation(id) {
+        return axios.get('http://localhost:8080/location/' + id, {timeout: 10000})
+    }
+
 }
 
 export {EventService}
