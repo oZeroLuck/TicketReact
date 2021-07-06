73a3de9e504712c4d7ee184e80528bb8c09e94f4
luigi.durso <fabiolo97.hu@gmail.com>
2021-07-06 11:37:21 +0200

    Fixed double navbar

--- src/pages/event-page.js
+++ src/pages/event-page.js
@@ -64,7 +64,6 @@ class EventPage extends React.Component {
 
         return (
             <div style={{backgroundColor: "#c9c9c9"}}>
-                <CustomNavbar/>
                 <Container fluid>
                     <Row style={{backgroundColor: "black"}}>
                         <Container>
