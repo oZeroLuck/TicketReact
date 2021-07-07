4a5489c5221622609746f474561040ed8ffd1d03
luigi.durso <fabiolo97.hu@gmail.com>
2021-07-07 15:32:52 +0200

    Removed test table

--- src/pages/admin/admin-homepage.js
+++ src/pages/admin/admin-homepage.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {CustomNavbar} from "../../components/custom-navbar";
-import {UserService} from "../../services/user-service";
 import {LoadingSpinner} from "../../components/loading-spinner";
 import {CustomTable} from "../../components/custom-table/custom-table";
-import {EventTbCfg, UserTbCfg} from "../../components/custom-table/table-cfg";
+import {EventTbCfg} from "../../components/custom-table/table-cfg";
 import {Container} from "react-bootstrap";
 import {EventService} from "../../services/event-service";
-import {LocationModal} from "../../components/location-modal";
+import {TicketService} from "../../services/ticket-service";
+import {InfoModalComponent} from "../../components/info-modal-component";
 
 class AdminHomepage extends React.Component {
     constructor(props) {
@@ -14,24 +14,20 @@ class AdminHomepage extends React.Component {
         this.state = {
             dataSource: null,
             isLoading: true,
-            dataSource2: null,
-            location: null,
             isLoadingLocation: false,
-            showMdl: false
+            showMdl: false,
+            modalData: null
         }
-        this.userService = new UserService()
         this.eventService = new EventService()
+        this.ticketSercive = new TicketService()
         this.handleClicked = this.handleClicked.bind(this)
         this.handleModal = this.handleModal.bind(this)
     }
 
     componentDidMount() {
-        this.userService.getUsers().then(result =>
-            this.setState({dataSource: result.data}, () =>
-                this.eventService.getEvents().then(data =>
-                this.setState({dataSource2: data.data, isLoading: false},
-                    () => console.log(this.state.dataSource2))))
-        )
+        this.eventService.getEvents().then(data =>
+            this.setState({dataSource: data.data, isLoading: false},
+                () => console.log(this.state.dataSource)))
 
     }
 
@@ -41,11 +37,37 @@ class AdminHomepage extends React.Component {
             this.setState({isLoadingLocation: true}, () =>
                 this.eventService.getLocation(itemId)
                     .then(response => {
+                        this.ticketSercive.getSold(itemId)
+                            .then(tickets => {
+                                this.setState({
+                                    isLoadingLocation:false,
+                                    modalData: {
+                                        location: response.data,
+                                        soldTicket: tickets.data,
+                                        error: false,
+                                        errorMsg: null
+                                    }
+                                }, () => this.handleModal())})
+                            .catch(error => {
+                                this.setState({
+                                    isLoadingLocation:false,
+                                    modalData: {
+                                        location: null,
+                                        soldTicket: null,
+                                        error: true,
+                                        errorMsg: error.message
+                                    }
+                                }, () => this.handleModal())})
+                    }).catch(error => {
                         this.setState({
-                            location: response.data,
-                            isLoadingLocation: false
-                        }, () => this.handleModal())
-                    }).catch(error => console.log(error.message))
+                            isLoadingLocation:false,
+                            modalData: {
+                                location: null,
+                                soldTicket: null,
+                                error: true,
+                                errorMsg: error.message
+                            }
+                }, () => this.handleModal())})
             )
         }
     }
@@ -53,11 +75,16 @@ class AdminHomepage extends React.Component {
     handleModal() {
         this.setState(prev => ({
             showMdl: !prev.showMdl
-        }), () => console.log(this.state))
+        }), () => {
+            if (!this.state.showMdl) {
+                this.setState({
+                    modalData: null
+                })
+            }
+        })
     }
 
     render() {
-        const tableCfg = new UserTbCfg()
         if(this.state.isLoading) {
             return <>
                 <CustomNavbar/>
@@ -66,19 +93,15 @@ class AdminHomepage extends React.Component {
         }
         return (
             <div>
-                <LocationModal show={this.state.showMdl}
-                               close={this.handleModal}
-                               data={this.state.location}
-                />
+                {this.state.modalData ?
+                    <InfoModalComponent show={this.state.showMdl}
+                                        close={this.handleModal}
+                                        data={this.state.modalData}/> : null}
                 <CustomNavbar/>
                 <h1 className={"text-center mt-5"}>Events list</h1>
-                <Container className={"mt-xl-5"}>
-                    <CustomTable dataSource={this.state.dataSource} tableCfg={tableCfg}/>
-                        I'm Admin Homepage and I'm working
-                </Container>
                 <Container>
                     {this.state.isLoadingLocation ? <LoadingSpinner/> : null}
-                    <CustomTable dataSource={this.state.dataSource2}
+                    <CustomTable dataSource={this.state.dataSource}
                                  tableCfg={EventTbCfg}
                                  parentCallback={this.handleClicked}
                     />
