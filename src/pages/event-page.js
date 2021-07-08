c5017db4149f2fe0b0b64b0db0870a4c06916dbc
luigi.durso <fabiolo97.hu@gmail.com>
2021-07-08 13:46:59 +0200

    Added function to add tickets to cart

--- src/pages/event-page.js
+++ src/pages/event-page.js
@@ -4,39 +4,76 @@ import {EventService} from "../services/event-service";
 import {Col, Container, Row} from "react-bootstrap";
 import {CustomNavbar} from "../components/custom-navbar";
 import "./pages.css"
-import {TicketList} from "../components/ticket-list/ticket-list";
 import {ErrorPage} from "./error-page";
 import {CustomSnackbar} from "../components/custom-snackbar";
+import {TicketComponent} from "../components/ticket-list/ticket-component";
+import {TicketService} from "../services/ticket-service";
+import {AddToCartBtn} from "../components/custom-button/btn-cfg";
 
 class EventPage extends React.Component {
+
     constructor(props) {
         super(props);
         this.eventApi = new EventService();
         this.state = {
             event: null,
             loading: true,
+            availableSeats: null,
             error: null,
             showSnack: false,
             snackMessage: null
         }
-        this.setSnack = this.setSnack.bind(this)
+        this.addToCart = this.addToCart.bind(this)
+        this.ticketService = new TicketService()
+        this.eventService = new EventService()
     }
 
     componentDidMount() {
-        this.eventApi.getFilm(this.props.match.params.id).then(result =>
+        const id = this.props.match.params.id
+        this.eventApi.getEvent(id).then(result =>
             this.setState({
-                event: result.data,
-                loading: false
-            })
+                event: result.data
+            }, () => this.calculateSeats(id))
         ).catch(error => {
             this.setState({loading: false, error: error.message})
         })
     }
 
-    setSnack(childData) {
-        console.log("Event-page setSnack!")
-        console.log(childData)
-        const message = "Ticket id: " + childData.id + "\nTicket seat: " + childData.seat
+    calculateSeats(id) {
+        this.eventService.getLocation(id).then(location =>
+            this.ticketService.getSold(id).then(sold => {
+                let available = []
+                location.data.seats.map(seat => {
+                    if (seat.max - sold.data[seat.name]) {
+                        available.push({name: seat.name, label: seat.label})
+                    }
+                    return null
+                })
+                this.setState({
+                    availableSeats: available,
+                    loading: false
+                })}
+            ))
+    }
+
+    addToCart(seat) {
+        console.log(this.state)
+        const ticket = {
+            eventId: this.state.event.id,
+            seat: seat
+        }
+        let newCart = JSON.parse(window.sessionStorage.getItem("currentCart"))
+        console.log(newCart)
+        newCart.push(ticket)
+        window.sessionStorage.setItem("currentCart", JSON.stringify(newCart))
+        if (window.sessionStorage.getItem("currentUser") !== null) {
+            console.log("save this to db")
+        }
+        const message = "Added to cart"
+        this.setSnack(message)
+    }
+
+    setSnack = function(message) {
         this.setState(prev => ({
             showSnack: !prev.showSnack, snackMessage: message
         }), () => {
@@ -67,13 +104,13 @@ class EventPage extends React.Component {
                 <CustomNavbar/>
                 <Container fluid>
                     <Row style={{backgroundColor: "black"}}>
-                        <Container>
+                        <Container style={{height: "fit-content"}}>
                             <Row>
-                                <Col className={"align-content-md-center"}>
+                                <Col className={"align-content-center justify-content-center"}>
                                     <img
                                          src={this.state.event.link}
                                          alt={this.state.event.id}
-                                         style={{padding: 20}}
+                                         className={"event-image"}
                                          />
                                 </Col>
                                 <Col className={"white-text"}
@@ -91,10 +128,14 @@ class EventPage extends React.Component {
                         </Container>
                     </Row>
                 </Container>
-                <CustomNavbar/>
+                <div style={{height: "3.5rem", backgroundColor: "#343a40"}}/>
                 <br/>
                 <Container>
-                    <TicketList route={this.props.location} onButtonPress={this.setSnack}/>
+                    <TicketComponent
+                        button={AddToCartBtn}
+                        data = {this.state.event}
+                        seats = {this.state.availableSeats}
+                        handleClick={this.addToCart}/>
                 </Container>
                 <CustomSnackbar show={this.state.showSnack}
                                 message={this.state.snackMessage}
